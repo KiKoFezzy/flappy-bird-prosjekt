ae608cfe9092f4574834b5dbe09469d80e284d22
Fezzy <victor1.26pg@gmail.com>
2026-06-09 14:54:55 +0200

    new highscores

--- obj/Debug/net10.0/flappy-bird prosjekt.AssemblyInfo.cs
+++ obj/Debug/net10.0/flappy-bird prosjekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("flappy-bird prosjekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92ec3613491a4b20898960050fc49cdf5117ec91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b0ab55b244a8514483d00edd4cae0a8fa578e7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("flappy-bird prosjekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("flappy-bird prosjekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
